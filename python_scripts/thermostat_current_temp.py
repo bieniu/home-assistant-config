1a19fbfcd2960e61b4661e815c4c659bcb19cd86
bieniu <maciej.bieniek@gmail.com>
2017-11-17 09:08:43 +0100

    Update thermostat script

--- python_scripts/thermostat_current_temp.py
+++ python_scripts/thermostat_current_temp.py
@@ -4,10 +4,11 @@ sensor_id  = data.get('sensor')
 if thermostat_id != "" and sensor_id != "":
    sensor = hass.states.get(sensor_id)
    temp = sensor.state
-   thermostat = hass.states.get(thermostat_id)
-   attributes = thermostat.attributes.copy()
-   attributes['current_temperature'] = temp
-   hass.states.set(thermostat_id, thermostat.state, attributes)
+   if temp is not None and temp is not "Unknown":
+      thermostat = hass.states.get(thermostat_id)
+      attributes = thermostat.attributes.copy()
+      attributes['current_temperature'] = temp
+      hass.states.set(thermostat_id, thermostat.state, attributes)
 else:
    logger.error("Wrong data.")
 
