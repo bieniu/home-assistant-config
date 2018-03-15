50a1053b355a61536cdbaf764094d5cc40b87919
Maciej Bieniek <bieniu@users.noreply.github.com>
2018-03-15 21:43:14 +0100

    Update update_thermostats.py

--- apps/update_thermostats.py
+++ apps/update_thermostats.py
@@ -6,9 +6,12 @@ Arguments:
  - heat_state			- name of heating state, default 'heat' (optional)
  - idle_state			- name of idle state, default 'idle' (optional)
  - idle_heat_temp		- temperature value between 'idle' and 'heat' states, default 8 (optional)
- - wait_for_zwave		- defines whether the script has to wait for the initialization of the Z-wave component, default True (optional)
-						  With wait_for_zwave = True script waits for zwave.network_ready event to start. You have to restart Home Assistant to generate this event.
-The order of thermostats and sensors is important. The first thermostat takes data from the first sensor, the second thermostat from the second sensor, etc.
+ - wait_for_zwave		- defines whether the script has to wait for the initialization of the Z-wave component,
+						  default True (optional)
+						  With wait_for_zwave = True script waits for zwave.network_ready event to start. You have
+						  to restart Home Assistant to generate this event.
+The order of thermostats and sensors is important. The first thermostat takes data from the first sensor, the second thermostat from
+the second sensor, etc.
 
 Configuration example:
 
@@ -61,12 +64,17 @@ class UpdateThermostats(appapi.AppDaemon):
 			self.idle_heat_temp = self.args['idle_heat_temp']
 		else:
 			self.idle_heat_temp = 8
+		if 'debug' in self.args:
+			if self.args['debug']:
+				self.log_level = 'DEBUG'
+		else:
+			self.log_level = 'INFO'
 
 		if wait_for_zwave:
 			self.log('Waiting for zwave.network_ready event...')
 			self.zwave_ready_handle = self.listen_event(self.start_listen_states, 'zwave.network_ready')
 		else:
-			self.start_listen_states()
+			self.start_listen_states(event = None, data = None, kwargs = None)
 
 	def start_listen_states(self, event, data, kwargs):
 		if self.zwave_ready_handle is not None:
@@ -79,10 +87,13 @@ class UpdateThermostats(appapi.AppDaemon):
 			if self.entity_exists(self.args['sensors'][i]) == False:
 				self.error('Wrong arguments! At least one of the entities does not exist.')
 				return
-			self.listen_state(self.thermostat_state_changed, self.args['thermostats'][i], attribute = 'current_temperature', new = None)
+			self.listen_state(self.thermostat_state_changed, self.args['thermostats'][i], attribute = 'current_temperature',
+							  new = None)
 			self.listen_state(self.sensor_state_changed, self.args['sensors'][i])
 			if self.get_state(self.args['thermostats'][i], attribute="current_temperature") == None:
-				self.thermostat_state_changed(self.args['thermostats'][i], attribute = "current_temperature", old = None, new = None, kwargs = None)
+				self.thermostat_state_changed(self.args['thermostats'][i], attribute = "current_temperature", old = None,
+											  new = None, kwargs = None)
+		self.log('Ready for action...')
 
 	def thermostat_state_changed(self, entity, attribute, old, new, kwargs):
 		for i in range(len(self.args['thermostats'])):
@@ -94,7 +105,7 @@ class UpdateThermostats(appapi.AppDaemon):
 
 		if sensor_temp is not None and sensor_temp != 'Unknown':
 			if new == None or float(new) != float(sensor_temp):
-				update_thermostat(entity, target_temp, sensor_temp)			
+				self.update_thermostat(entity, target_temp, sensor_temp)
 		else:
 			self.log('No temperature data on the sensor {}.'.format(sensor_id))
 
@@ -108,16 +119,15 @@ class UpdateThermostats(appapi.AppDaemon):
 
 		if new is not None and new != 'Unknown':
 			if current_temp == None or float(current_temp) != float(new):
-				update_temperature(thermostat_id, target_temp, new)
+				self.update_thermostat(thermostat_id, target_temp, new)
 		else:
 			self.log('No temperature data on the sensor {}.'.format(entity))
-				
-	def update_thermostat(entity, target_temp, current_temp, kwargs):
+
+	def update_thermostat(self, entity, target_temp, current_temp):
 			self.find_thermostat_state(float(target_temp))
-			self.log('Updating state and current temperature for {}...'.format(entity), level='DEBUG')
+			self.log('Updating state and current temperature for {}...'.format(entity), self.log_level)
 			self.set_state(entity, state=self.state, attributes = {"current_temperature": current_temp})
-		
-		
+
 	def find_thermostat_state(self, target_temp):
 		if target_temp > self.idle_heat_temp:
 			self.state = self.heat_state
