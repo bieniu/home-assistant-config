8a0d74a3d30c4de74632b3f96a41e85014a01f6d
Bieniu <maciej.bieniek@gmail.com>
2019-02-27 23:21:11 +0100

    Update Shellies Discovery script

--- python_scripts/shellies_discovery.py
+++ python_scripts/shellies_discovery.py
@@ -1,6 +1,6 @@
 """
 This script adds MQTT discovery support for Shellies. Shelly1, Shelly2,
-Shelly4Pro and Shelly Plug are supported.
+Shelly4Pro, Shelly Plug and Shelly Plug are supported.
 Arguments:
  - discovery_prefix:    - discovery prefix in HA, default 'homeassistant',
                           optional
@@ -8,7 +8,9 @@ Arguments:
  - mac                  - Shelly MAC address (required)
  - sensor				- sensor entity_id (required)
  - fw_ver               - Shelly firmware version (optional)
- - list of shelies relays and components for them
+ - temp_unit            - C for Celsius, F for Farenhait, default C (optional)
+ - list of shelies relays and components for them, this is required only for
+                        devices with relays
 
 Configuration example:
 Automations:
@@ -35,6 +37,7 @@ Automations:
       id: '{{ trigger.payload_json.id }}'
       mac: '{{ trigger.payload_json.mac }}'
       fw_ver: '{{ trigger.payload_json.fw_ver }}'
+      temp_unit: 'F'
       shelly1-328900-relay-0: 'light'
       shellyswitch-5B2604-relay-0: 'switch'
       shellyswitch-5B2604-relay-1: 'light'
@@ -57,7 +60,7 @@ custom_updater:
     - https://raw.githubusercontent.com/bieniu/home-assistant-config/master/python_scripts/python_scripts.json
 """
 
-VERSION = '0.1.1'
+VERSION = '0.2.0'
 
 ATTR_DEVELOP = 'develop'
 
@@ -65,6 +68,7 @@ ATTR_ID = 'id'
 ATTR_MAC = 'mac'
 ATTR_FW_VER = 'fw_ver'
 ATTR_DISCOVERY_PREFIX = 'discovery_prefix'
+ATTR_TEMP_UNIT = 'temp_unit'
 
 develop = False
 retain = True
@@ -72,6 +76,10 @@ retain = True
 id = data.get(ATTR_ID)
 mac = data.get(ATTR_MAC)
 fw_ver = data.get(ATTR_FW_VER)
+temp_unit = '°C'
+if data.get(ATTR_TEMP_UNIT) is not None:
+    if data.get(ATTR_TEMP_UNIT) == 'F':
+        temp_unit = '°F'
 disc_prefix = 'homeassistant'
 if data.get(ATTR_DISCOVERY_PREFIX) is not None:
     disc_prefix = data.get(ATTR_DISCOVERY_PREFIX)
@@ -88,6 +96,7 @@ else:
     relays = 0
     relay_sensors = []
     relay_components = ['switch', 'light', 'fan']
+    sensors = []
 
     if 'shelly1' in id:
         model = 'Shelly1'
@@ -108,7 +117,7 @@ else:
         relays = 1
         relay_sensors = ['power', 'energy']
         units = ['W', 'kWh']
-        templates = ['{{ value | round }}', '{{ (value / 10000) | round(2) }}']
+        templates = ['{{ value | round }}', '{{ (value | float / 10000) | round(2) }}']
 
     if 'shelly4pro' in id:
         model = 'Shelly4Pro'
@@ -121,6 +130,8 @@ else:
     if 'shellyht' in id:
         model = 'ShellyH&T'
         sensors = ['temperature', 'humidity', 'battery']
+        units = [temp_unit, '%', '%']
+        templates = ['{{ value | round(1) }}', '{{ value | round(1) }}', '{{ value | round }}']
 
     for relay_id in range(0, relays):
         device_name = '{} {}'.format(model, id.split('-')[1],)
@@ -163,9 +174,9 @@ else:
                     unique_id = '{}-relay-{}'.format(id,
                                                      relay_sensors[sensor_id])
                     config_topic = '{}/sensor/{}-{}/config'.format(disc_prefix,
-                                                   id, relay_sensors[sensor_id])
+                                                  id, relay_sensors[sensor_id])
                     sensor_name = '{} {}'.format(device_name,
-                                          relay_sensors[sensor_id].capitalize())
+                                         relay_sensors[sensor_id].capitalize())
                     state_topic =  '~relay/{}'.format(relay_sensors[sensor_id])
                     payload = '{\"name\":\"' + sensor_name + '\",' \
                               '\"stat_t\":\"' + state_topic + '\",' \
@@ -191,13 +202,13 @@ else:
         else:
             for sensor_id in range(0, len(relay_sensors)):
                 unique_id = '{}-relay-{}-{}'.format(id,
-                                             relay_sensors[sensor_id], relay_id)
+                                            relay_sensors[sensor_id], relay_id)
                 config_topic = '{}/sensor/{}-{}-{}/config'.format(disc_prefix,
-                                         id, relay_sensors[sensor_id], relay_id)
+                                        id, relay_sensors[sensor_id], relay_id)
                 sensor_name = '{} {} {}'.format(device_name,
-                                relay_sensors[sensor_id].capitalize(), relay_id)
+                               relay_sensors[sensor_id].capitalize(), relay_id)
                 state_topic =  '~relay/{}/{}'.format(relay_id,
-                                                       relay_sensors[sensor_id])
+                                                      relay_sensors[sensor_id])
                 payload = '{\"name\":\"' + sensor_name + '\",' \
                           '\"stat_t\":\"' + state_topic + '\",' \
                           '\"unit_of_meas\":\"' + units[sensor_id] + '\",' \
@@ -219,3 +230,35 @@ else:
                     'qos': 0
                 }
                 hass.services.call('mqtt', 'publish', service_data, False)
+    for sensor_id in range(0, len(sensors)):
+        device_name = '{} {}'.format(model, id.split('-')[1],)
+        unique_id = '{}-{}'.format(id, sensors[sensor_id])
+        config_topic = '{}/sensor/{}-{}/config'.format(disc_prefix, id,
+                                                       sensors[sensor_id])
+        default_topic = 'shellies/{}/'.format(id)
+        availability_topic = '~online'
+        sensor_name = '{} {}'.format(device_name,
+                                     sensors[sensor_id].capitalize())
+        state_topic =  '~sensor/{}'.format(sensors[sensor_id])
+        payload = '{\"name\":\"' + sensor_name + '\",' \
+                  '\"stat_t\":\"' + state_topic + '\",' \
+                  '\"unit_of_meas\":\"' + units[sensor_id] + '\",' \
+                  '\"device_class\":\"' + sensors[sensor_id] + '\",' \
+                  '\"val_tpl\":\"' + templates[sensor_id] + '\",' \
+                  '\"avty_t\":\"' + availability_topic + '\",' \
+                  '\"pl_avail\":\"true\",' \
+                  '\"pl_not_avail\":\"false\",' \
+                  '\"uniq_id\":\"' + unique_id + '\",' \
+                  '\"device\": {\"identifiers\": [\"' + mac + '\"],' \
+                  '\"name\":\"' + device_name + '\",' \
+                  '\"model\":\"' + model + '\",' \
+                  '\"sw_version\":\"' + fw_ver + '\",' \
+                  '\"manufacturer\":\"Shelly\"},' \
+                  '\"~\":\"' + default_topic + '\"}'
+        service_data = {
+            'topic': config_topic,
+            'payload': payload,
+            'retain': retain,
+            'qos': 0
+        }
+        hass.services.call('mqtt', 'publish', service_data, False)
