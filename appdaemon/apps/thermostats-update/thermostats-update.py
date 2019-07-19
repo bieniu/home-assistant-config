28a9942aaa9a121bf82121091c1839e936b663d2
Bieniu <maciej.bieniek@gmail.com>
2019-07-19 20:01:36 +0200

    Update HA to 0.96.x

--- appdaemon/apps/thermostats-update/thermostats-update.py
+++ appdaemon/apps/thermostats-update/thermostats-update.py
@@ -11,6 +11,9 @@ Arguments:
                      default 8 (optional)
  - state_only      - with state_only set to 'true' app will update only
                      state of the thermostat, default false (optional)
+ - temp_only       - with temp_only set to 'true' app will update only
+                     current_temperature of the thermostat, default false
+                     (optional)
  - wait_for_zwave  - defines whether the script has to wait for the
                      initialization of the Z-wave component
                      after Home Assistant restart, default True (optional)
@@ -18,8 +21,8 @@ Arguments:
 Configuration example:
 
 thermostats_update:
-  module: heating_thermostats_update
-  class: HeatingThermostatsUpdate
+  module: thermostats-update
+  class: ThermostatsUpdate
   rooms:
     kitchen:
       thermostat: climate.thermostat_kitchen
@@ -45,7 +48,7 @@ class ThermostatsUpdate(hass.Hass):
 
     def initialize(self):
 
-        __version__ = '0.3.0'
+        __version__ = '0.3.1'
 
         ATTR_HEAT_DEFAULT = 'heat'
         ATTR_OFF_DEFAULT = 'off'
@@ -58,6 +61,7 @@ class ThermostatsUpdate(hass.Hass):
         ATTR_IDLE_STATE = 'idle_state'
         ATTR_IDLE_HEAT_TEMP = 'idle_heat_temp'
         ATTR_STATE_ONLY = 'state_only'
+        ATTR_TEMP_ONLY = 'temp_only'
 
         self.zwave_handle = None
 
@@ -69,6 +73,14 @@ class ThermostatsUpdate(hass.Hass):
             self.state_only = self.args[ATTR_STATE_ONLY]
         else:
             self.state_only = False
+        if ATTR_TEMP_ONLY in self.args:
+            self.temp_only = self.args[ATTR_TEMP_ONLY]
+        else:
+            self.temp_only = False
+        if self.temp_only and self.state_only:
+            self.error("You can use state_only: true or temp_only: true not "
+                       "both!")
+            return
         if ATTR_HEAT_STATE in self.args:
             self.heat_state = self.args[ATTR_HEAT_STATE]
         else:
@@ -137,16 +149,19 @@ class ThermostatsUpdate(hass.Hass):
                     sensor_id = self.args['rooms'][room]['sensor']
 
         target_temp = self.get_state(entity, attribute='temperature')
+        org_state = self.get_state(entity)
         if not self.state_only:
             sensor_temp = self.get_state(sensor_id)
             if sensor_temp and sensor_temp != 'unknown':
                 if not new or (new != 'unknown'
                             and float(new) != float(sensor_temp)):
-                    self.update_thermostat(entity, target_temp, sensor_temp)
+                    self.update_thermostat(entity, org_state, target_temp,
+                                           sensor_temp)
             else:
                 self.log("No temperature data on the sensor {}.".format(sensor_id))
         else:
-            self.update_thermostat(entity, target_temp, None)
+            self.update_thermostat(entity, org_state, target_temp, None)
+
     def sensor_state_changed(self, entity, attribute, old, new, kwargs):
         for room in self.args['rooms']:
             if entity == self.args['rooms'][room]['sensor']:
@@ -155,22 +170,28 @@ class ThermostatsUpdate(hass.Hass):
         current_temp = self.get_state(thermostat_id,
                                       attribute='current_temperature')
         target_temp = self.get_state(thermostat_id, attribute='temperature')
-
+        org_state = self.get_state(thermostat_id)
         if new and new != 'unknown':
             if not current_temp or (current_temp != 'unknown'
                                     and float(current_temp) != float(new)):
-                self.update_thermostat(thermostat_id, target_temp, new)
+                self.update_thermostat(thermostat_id, org_state, target_temp,
+                                       new)
         else:
             self.log("No temperature data on the sensor {}.".format(entity))
 
-    def update_thermostat(self, entity, target_temp, current_temp):
+    def update_thermostat(self, entity, state, target_temp, current_temp):
         self.log("Updating state and current "
                  "temperature for {}...".format(entity), self.log_level)
         if current_temp:
-            self.set_state(
-                entity,
-                state=self.find_thermostat_state(float(target_temp)),
-                attributes={"current_temperature": current_temp})
+            if not self.temp_only:
+                self.set_state(
+                    entity,
+                    state=self.find_thermostat_state(float(target_temp)),
+                    attributes={"current_temperature": current_temp})
+            else:
+                self.set_state(
+                    entity, state=state,
+                    attributes={"current_temperature": current_temp})
         else:
             self.set_state(
                 entity,
