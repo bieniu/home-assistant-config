a36e4b3c28e7c83fdf6d89ed5937b1d1f23eb872
Bieniu <maciej.bieniek@gmail.com>
2019-09-04 22:04:46 +0200

    Prettier

--- appdaemon/apps/brother-printer-status/brother-printer-status.py
+++ appdaemon/apps/brother-printer-status/brother-printer-status.py
@@ -35,10 +35,9 @@ import json
 
 
 class BrotherPrinterStatus(hass.Hass):
-
     def initialize(self):
 
-        __version__ = '0.3.2'
+        __version__ = "0.3.2"
 
         # max value of the height of the black image on the printer's webpage
         self.MAX_IMAGE_HEIGHT = 56
@@ -46,61 +45,62 @@ class BrotherPrinterStatus(hass.Hass):
         self.STATUS_URL = "/general/status.html"
         self.TOPIC = "printer/{}/"
         self.SENSOR_PREFIX = "sensor.printer_{}"
-        self.MANUFACTURER = 'Brother'
+        self.MANUFACTURER = "Brother"
         self.use_mqtt = False
         self.mac = None
         self.mac_simple = None
         self.retain = False
 
         try:
-            if self.args['host']:
-                self.host = self.args['host']
+            if self.args["host"]:
+                self.host = self.args["host"]
         except KeyError:
-            self.error("Wrong arguments! You must supply a valid printer "
-                       "hostname or IP address.")
+            self.error(
+                "Wrong arguments! You must supply a valid printer "
+                "hostname or IP address."
+            )
             return
         try:
-            if 'status_interval' in self.args:
-                status_interval = int(self.args['status_interval'])
+            if "status_interval" in self.args:
+                status_interval = int(self.args["status_interval"])
             else:
                 status_interval = 10
         except ValueError:
-            self.error("Wrong arguments! Argument status_interval has to be "
-                       "an integer.")
+            self.error(
+                "Wrong arguments! Argument status_interval has to be " "an integer."
+            )
             return
         try:
-            if 'info_interval' in self.args:
-                info_interval = int(self.args['info_interval'])
+            if "info_interval" in self.args:
+                info_interval = int(self.args["info_interval"])
             else:
                 info_interval = 300
         except ValueError:
-            self.error("Wrong arguments! Argument info_interval has to be "
-                       "an integer.")
+            self.error(
+                "Wrong arguments! Argument info_interval has to be " "an integer."
+            )
             return
-        if 'use_mqtt' in self.args:
-            self.use_mqtt = self.args['use_mqtt']
+        if "use_mqtt" in self.args:
+            self.use_mqtt = self.args["use_mqtt"]
             if self.use_mqtt:
-                if 'retain' in self.args:
-                    self.retain = self.args['retain']
-                if 'mac' in self.args:
-                    self.mac = self.args['mac']
-                    self.mac_simple = self.mac.replace(':', '').lower()
+                if "retain" in self.args:
+                    self.retain = self.args["retain"]
+                if "mac" in self.args:
+                    self.mac = self.args["mac"]
+                    self.mac_simple = self.mac.replace(":", "").lower()
 
-        self.run_every(self.update_printer_status_page, datetime.now(),
-                       status_interval)
-        self.run_every(self.update_printer_info_page, datetime.now(),
-                       info_interval)
+        self.run_every(self.update_printer_status_page, datetime.now(), status_interval)
+        self.run_every(self.update_printer_info_page, datetime.now(), info_interval)
 
     def update_printer_status_page(self, kwargs):
-        page = self.download_page("http://{}{}".format(self.host,
-                                                       self.STATUS_URL))
+        page = self.download_page("http://{}{}".format(self.host, self.STATUS_URL))
 
         if page:
             regex_res = self.regex(r"<dd>.*>(\w+\s?\w+)\s+<.*</dd>", page)
             if regex_res:
                 status = regex_res.lower()
                 if status:
-                    sensor = 'status'
+                    sensor = "status"
                     entity = self.SENSOR_PREFIX.format(sensor)
                     if self.use_mqtt:
                         topic = self.TOPIC.format(sensor)
@@ -113,27 +113,26 @@ class BrotherPrinterStatus(hass.Hass):
                                 "cns": [["mac", self.mac]],
                                 "mf": self.MANUFACTURER,
                                 "mdl": model,
-                                "name": self.MANUFACTURER + ' ' + model
+                                "name": self.MANUFACTURER + " " + model,
                             },
                             "ic": "mdi:printer",
-                            "stat_t": topic + "state"
+                            "stat_t": topic + "state",
                         }
-                        self.mqtt_publish(topic, payload, entity, sensor,
-                                          status)
+                        self.mqtt_publish(topic, payload, entity, sensor, status)
                     else:
-                        attributes = {"friendly_name": "Printer status",
-                                      "icon": "mdi:printer"}
-                        self.set_state(entity, state=status,
-                                       attributes=attributes)
-            regex_res = self.regex(r"class=\"tonerremain\" height=\"(\d+)\"",
-                                   page)
+                        attributes = {
+                            "friendly_name": "Printer status",
+                            "icon": "mdi:printer",
+                        }
+                        self.set_state(entity, state=status, attributes=attributes)
+            regex_res = self.regex(r"class=\"tonerremain\" height=\"(\d+)\"", page)
             if regex_res:
                 try:
                     toner = round(int(regex_res) / self.MAX_IMAGE_HEIGHT * 100)
                 except TypeError:
                     return
                 if toner:
-                    sensor = 'toner'
+                    sensor = "toner"
                     entity = self.SENSOR_PREFIX.format(sensor)
                     if self.use_mqtt:
                         topic = self.TOPIC.format(sensor)
@@ -146,24 +145,23 @@ class BrotherPrinterStatus(hass.Hass):
                                 "cns": [["mac", self.mac]],
                                 "mf": self.MANUFACTURER,
                                 "mdl": model,
-                                "name": self.MANUFACTURER + ' ' + model
+                                "name": self.MANUFACTURER + " " + model,
                             },
                             "ic": "mdi:flask-outline",
                             "unit_of_meas": "%",
-                            "stat_t": topic + "state"
+                            "stat_t": topic + "state",
                         }
-                        self.mqtt_publish(topic, payload, entity, sensor,
-                                          toner)
+                        self.mqtt_publish(topic, payload, entity, sensor, toner)
                     else:
-                        attributes = {"friendly_name": "Printer " + sensor,
-                                      "icon": "mdi:flask-outline",
-                                      "unit_of_measurement": "%"}
-                        self.set_state(entity, state=toner,
-                                       attributes=attributes)
+                        attributes = {
+                            "friendly_name": "Printer " + sensor,
+                            "icon": "mdi:flask-outline",
+                            "unit_of_measurement": "%",
+                        }
+                        self.set_state(entity, state=toner, attributes=attributes)
 
     def update_printer_info_page(self, kwargs):
-        page = self.download_page("http://{}{}".format(self.host,
-                                                       self.INFO_URL))
+        page = self.download_page("http://{}{}".format(self.host, self.INFO_URL))
         if page:
             regex_res = self.regex(r"<dd>(\d+)</dd>", page)
             if regex_res:
@@ -172,7 +170,7 @@ class BrotherPrinterStatus(hass.Hass):
                 except TypeError:
                     return
                 if counter:
-                    sensor = 'counter'
+                    sensor = "counter"
                     entity = self.SENSOR_PREFIX.format(sensor)
                     if self.use_mqtt:
                         topic = self.TOPIC.format(sensor)
@@ -185,20 +183,20 @@ class BrotherPrinterStatus(hass.Hass):
                                 "cns": [["mac", self.mac]],
                                 "mf": self.MANUFACTURER,
                                 "mdl": model,
-                                "name": self.MANUFACTURER + ' ' + model
+                                "name": self.MANUFACTURER + " " + model,
                             },
                             "ic": "mdi:file-document",
                             "unit_of_meas": "p",
-                            "stat_t": topic + "state"
+                            "stat_t": topic + "state",
                         }
-                        self.mqtt_publish(topic, payload, entity, sensor,
-                                          counter)
+                        self.mqtt_publish(topic, payload, entity, sensor, counter)
                     else:
-                        attributes = {"friendly_name": "Printer " + sensor,
-                                      "icon": "mdi:file-document",
-                                      "unit_of_measurement": "p"}
-                        self.set_state(entity, state=counter,
-                                       attributes=attributes)
+                        attributes = {
+                            "friendly_name": "Printer " + sensor,
+                            "icon": "mdi:file-document",
+                            "unit_of_measurement": "p",
+                        }
+                        self.set_state(entity, state=counter, attributes=attributes)
             regex_res = self.regex(r"\((\d+\.\d+)%\)", page)
             if regex_res:
                 try:
@@ -206,44 +204,40 @@ class BrotherPrinterStatus(hass.Hass):
                 except TypeError:
                     return
                 if drum_usage:
-                    sensor = 'drum-usage'
-                    entity = self.SENSOR_PREFIX.format(
-                        sensor.replace('-', '_'))
+                    sensor = "drum-usage"
+                    entity = self.SENSOR_PREFIX.format(sensor.replace("-", "_"))
                     if self.use_mqtt:
                         topic = self.TOPIC.format(sensor)
                         model = self.printer_model(page)
                         payload = {
-                            "name": "Printer " + sensor.replace('-', ' '),
+                            "name": "Printer " + sensor.replace("-", " "),
                             "uniq_id": self.mac_simple + "-" + sensor,
                             "dev": {
                                 "ids": self.mac_simple,
                                 "cns": [["mac", self.mac]],
                                 "mf": self.MANUFACTURER,
                                 "mdl": model,
-                                "name": self.MANUFACTURER + ' ' + model
+                                "name": self.MANUFACTURER + " " + model,
                             },
                             "ic": "mdi:chart-donut",
                             "unit_of_meas": "%",
-                            "stat_t": topic + "state"
+                            "stat_t": topic + "state",
                         }
-                        self.mqtt_publish(topic, payload, entity, sensor,
-                                          drum_usage)
+                        self.mqtt_publish(topic, payload, entity, sensor, drum_usage)
                     else:
-                        attributes = {"friendly_name": "Printer " +
-                                                       sensor.replace(
-                                                           '-', ' '),
-                                      "icon": "mdi:chart-donut",
-                                      "unit_of_measurement": "%"}
-                        self.set_state(entity, state=drum_usage,
-                                       attributes=attributes)
+                        attributes = {
+                            "friendly_name": "Printer " + sensor.replace("-", " "),
+                            "icon": "mdi:chart-donut",
+                            "unit_of_measurement": "%",
+                        }
+                        self.set_state(entity, state=drum_usage, attributes=attributes)
 
     def download_page(self, url):
         page = None
         try:
             page = requests.get(url, timeout=2)
         except requests.exceptions.RequestException:
-            self.error("Host {} unreachable or respond too "
-                       "slow!".format(self.host))
+            self.error("Host {} unreachable or respond too " "slow!".format(self.host))
             return
         return page.text
 
@@ -255,17 +249,25 @@ class BrotherPrinterStatus(hass.Hass):
 
     def mqtt_publish(self, topic, payload, entity, sensor, new_state):
         if self.mac_simple and not self.entity_exists(entity):
-            self.call_service("mqtt/publish", topic="homeassistant/sensor/" +
-                                                    topic+"config",
-                              payload=json.dumps(payload), qos=0,
-                              retain=True)
+            self.call_service(
+                "mqtt/publish",
+                topic="homeassistant/sensor/" + topic + "config",
+                payload=json.dumps(payload),
+                qos=0,
+                retain=True,
+            )
         old_state = self.get_state(entity)
         if old_state and str(old_state) != str(new_state):
-            self.call_service("mqtt/publish", topic=topic+"state",
-                              payload=new_state, qos=0, retain=self.retain)
+            self.call_service(
+                "mqtt/publish",
+                topic=topic + "state",
+                payload=new_state,
+                qos=0,
+                retain=self.retain,
+            )
 
     def printer_model(self, page):
-        model = 'Printer'
+        model = "Printer"
         regex_res = self.regex(r"<title>Brother\s(.+)\sseries</title>", page)
         if regex_res:
             model = regex_res
--- appdaemon/apps/dune-activity/dune-activity.py
+++ appdaemon/apps/dune-activity/dune-activity.py
@@ -30,88 +30,93 @@ import json
 
 
 class DuneActivity(hass.Hass):
-
     def initialize(self):
 
-        __version__ = '0.2.4'
+        __version__ = "0.2.4"
 
         self.URL_FORMAT = "http://{}/cgi-bin/do"
-        self.ENTITY = 'sensor.dune_activity'
+        self.ENTITY = "sensor.dune_activity"
         self.TOPIC = "dune/activity/{}"
-        self.MANUFACTURER = 'Dune'
-        self.model = 'Network Player'
+        self.MANUFACTURER = "Dune"
+        self.model = "Network Player"
         self.use_mqtt = False
         self.mac = None
         self.mac_simple = None
         self.retain = False
         try:
-            if self.args['host']:
-                self.host = self.args['host']
+            if self.args["host"]:
+                self.host = self.args["host"]
         except KeyError:
-            self.error("Wrong arguments! You must supply a valid DuneHD "
-                       "media player hostname or IP address.")
+            self.error(
+                "Wrong arguments! You must supply a valid DuneHD "
+                "media player hostname or IP address."
+            )
             return
         try:
-            if 'interval' in self.args:
-                interval = int(self.args['interval'])
+            if "interval" in self.args:
+                interval = int(self.args["interval"])
             else:
                 interval = 60
         except ValueError:
-            self.error("Wrong arguments! Argument interval has to be an "
-                       "integer.")
+            self.error("Wrong arguments! Argument interval has to be an " "integer.")
             return
-        if 'model' in self.args:
-            self.model = self.args['model']
-        if 'use_mqtt' in self.args:
-            self.use_mqtt = self.args['use_mqtt']
+        if "model" in self.args:
+            self.model = self.args["model"]
+        if "use_mqtt" in self.args:
+            self.use_mqtt = self.args["use_mqtt"]
             if self.use_mqtt:
-                if 'retain' in self.args:
-                    self.retain = self.args['retain']
-                if 'mac' in self.args:
-                    self.mac = self.args['mac']
-                    self.mac_simple = self.mac.replace(':', '').lower()
+                if "retain" in self.args:
+                    self.retain = self.args["retain"]
+                if "mac" in self.args:
+                    self.mac = self.args["mac"]
+                    self.mac_simple = self.mac.replace(":", "").lower()
         self.run_every(self.update_activity, datetime.now(), interval)
 
     def update_activity(self, kwargs):
         request = None
         try:
-            request = requests.get(self.URL_FORMAT.format(self.host),
-                                   timeout=2)
+            request = requests.get(self.URL_FORMAT.format(self.host), timeout=2)
         except requests.exceptions.RequestException:
             pass
         if not request:
-            state = 'offline'
+            state = "offline"
         elif request.status_code == 200:
-            pattern = re.compile(".*name=\"player_state\" value=\"(.*)\"")
+            pattern = re.compile('.*name="player_state" value="(.*)"')
             try:
                 state = re.findall(pattern, request.text)[0]
             except IndexError:
                 return
         else:
-            state = 'offline'
+            state = "offline"
         if self.use_mqtt:
             if self.mac_simple and not self.entity_exists(self.ENTITY):
                 payload = {
                     "name": "Dune Activity",
-                    "uniq_id": self.mac_simple + '-activity',
-                    "stat_t": self.TOPIC.format('state'),
+                    "uniq_id": self.mac_simple + "-activity",
+                    "stat_t": self.TOPIC.format("state"),
                     "dev": {
                         "ids": self.mac_simple,
                         "cns": [["mac", self.mac]],
                         "mf": self.MANUFACTURER,
                         "mdl": self.model,
-                        "name": self.MANUFACTURER + ' ' + self.model
-                    }
+                        "name": self.MANUFACTURER + " " + self.model,
+                    },
                 }
-                self.call_service("mqtt/publish",
-                                  topic='homeassistant/sensor/' +
-                                        self.TOPIC.format('config'),
-                                  payload=json.dumps(payload), qos=0,
-                                  retain=True)
+                self.call_service(
+                    "mqtt/publish",
+                    topic="homeassistant/sensor/" + self.TOPIC.format("config"),
+                    payload=json.dumps(payload),
+                    qos=0,
+                    retain=True,
+                )
             old_state = self.get_state(self.ENTITY)
             if old_state and old_state != state:
-                self.call_service("mqtt/publish",
-                                  topic=self.TOPIC.format('state'),
-                                  payload=state, qos=0, retain=self.retain)
+                self.call_service(
+                    "mqtt/publish",
+                    topic=self.TOPIC.format("state"),
+                    payload=state,
+                    qos=0,
+                    retain=self.retain,
+                )
         else:
             self.set_state(self.ENTITY, state=state)
