4338bc91e096d3ecd19a311da49b51275fbe003a
Bieniu <maciej.bieniek@gmail.com>
2020-02-11 15:27:36 +0100

    Update notify script

--- python_scripts/notify.py
+++ python_scripts/notify.py
@@ -1,5 +1,20 @@
+ATTR_ANDROID = "android"
+ATTR_IOS = "ios"
+ATTR_SERVICE = "service"
+ATTR_TYPE = "type"
+
+USER_EDYTA = "edyta"
+USER_MACIEK = "maciek"
+
+USERS = [USER_EDYTA, USER_MACIEK]
+
 NOTIFY_SERVICE_EDYTA = "notify.mobile_app_iphone_8_plus"
 NOTIFY_SERVICE_MACIEK = "notify.mobile_app_oneplus_6"
+
+SERVICES = {
+    USER_EDYTA: {ATTR_TYPE: ATTR_IOS, ATTR_SERVICE: NOTIFY_SERVICE_EDYTA},
+    USER_MACIEK: {ATTR_TYPE: ATTR_ANDROID, ATTR_SERVICE: NOTIFY_SERVICE_MACIEK},
+}
 
 CONF_ACTIONS = "actions"
 CONF_IMAGE = "image"
@@ -9,12 +24,12 @@ CONF_TAG = "tag"
 CONF_TITLE = "title"
 CONF_URL = "url"
 
-USER_EDYTA = "edyta"
-USER_MACIEK = "maciek"
+PRIORITY_NORMAL = "normal"
+PRIORITY_HIGH = "high"
 
-PRIORITIES = ["normal", "high"]
+PRIORITIES = [PRIORITY_NORMAL, PRIORITY_HIGH]
 
-priority = "normal"
+priority = PRIORITY_NORMAL
 if data.get(CONF_PRIORITY):
     priority = data.get(CONF_PRIORITY)
 if not priority in PRIORITIES:
@@ -32,58 +47,62 @@ if not title:
 if not message:
     raise ValueError("Message argument is empty")
 
-edyta = False
-if data.get(USER_EDYTA):
-    edyta = data.get(USER_EDYTA)
-if not isinstance(edyta, bool):
-    raise ValueError("Wrong value for edyta argument")
+for user in USERS:
+    notify_user = False
+    if data.get(user):
+        notify_user = data.get(user)
+    if not isinstance(notify_user, bool):
+        raise ValueError(f"Wrong value for {user} argument")
 
-maciek = False
-if data.get(USER_MACIEK):
-    maciek = data.get(USER_MACIEK)
-if not isinstance(maciek, bool):
-    raise ValueError("Wrong value for maciek argument")
+    if notify_user:
+        if SERVICES[user][ATTR_TYPE] == ATTR_IOS:
+            service_data = {"title": title, "message": message}
+            if image:
+                if not service_data.get("data"):
+                    service_data["data"] = {}
+                service_data["data"]["attachment"] = {}
+                service_data["data"]["attachment"]["url"] = image
+                service_data["data"]["attachment"]["content-type"] = "jpg"
+                service_data["data"]["attachment"]["hide-thumbnail"] = "false"
+            if tag:
+                if not service_data.get("data"):
+                    service_data["data"] = {}
+                service_data["data"]["apns_headers"] = {}
+                service_data["data"]["apns_headers"]["apns-collapse-id"] = tag
 
-if edyta:
-    service_data = {"title": title, "message": message}
-    if image:
-        if not service_data.get("data"):
-            service_data["data"] = {}
-        service_data["data"]["attachment"] = {}
-        service_data["data"]["attachment"]["url"] = image
-        service_data["data"]["attachment"]["content-type"] = "jpg"
-        service_data["data"]["attachment"]["hide-thumbnail"] = "false"
-    if tag:
-        if not service_data.get("data"):
-            service_data["data"] = {}
-        service_data["data"]["apns_headers"] = {}
-        service_data["data"]["apns_headers"]["apns-collapse-id"] = tag
+            logger.error(
+                f"service: {SERVICES[user][ATTR_SERVICE]}, data: {service_data}"
+            )
 
-    logger.debug(f"service: {NOTIFY_SERVICE_EDYTA}, data: {service_data}")
+            hass.services.call(
+                SERVICES[user][ATTR_SERVICE].split(".")[0],
+                SERVICES[user][ATTR_SERVICE].split(".")[1],
+                service_data,
+                False,
+            )
 
-    hass.services.call(
-        NOTIFY_SERVICE_EDYTA.split(".")[0],
-        NOTIFY_SERVICE_EDYTA.split(".")[1],
-        service_data,
-        False,
-    )
+        if SERVICES[user][ATTR_TYPE] == ATTR_ANDROID:
+            service_data = {
+                "title": title,
+                "message": message,
+                "data": {"priority": priority},
+            }
+            if actions:
+                service_data["data"]["actions"] = actions
+            if tag:
+                service_data["data"]["tag"] = tag
+            if url:
+                service_data["data"]["url"] = url
+            if image:
+                service_data["data"]["image"] = image
 
-if maciek:
-    service_data = {"title": title, "message": message, "data": {"priority": priority}}
-    if actions:
-        service_data["data"]["actions"] = actions
-    # if tag:
-    #     service_data["data"]["tag"] = tag
-    if url:
-        service_data["data"]["url"] = url
-    if image:
-        service_data["data"]["image"] = image
+            logger.error(
+                f"service: {SERVICES[user][ATTR_SERVICE]}, data: {service_data}"
+            )
 
-    logger.debug(f"service: {NOTIFY_SERVICE_MACIEK}, data: {service_data}")
-
-    hass.services.call(
-        NOTIFY_SERVICE_MACIEK.split(".")[0],
-        NOTIFY_SERVICE_MACIEK.split(".")[1],
-        service_data,
-        False,
-    )
+            hass.services.call(
+                SERVICES[user][ATTR_SERVICE].split(".")[0],
+                SERVICES[user][ATTR_SERVICE].split(".")[1],
+                service_data,
+                False,
+            )
